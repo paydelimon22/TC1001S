46653dca6bd8340541792a652bcff8520f8dbb13
BassedWarrior <bassedwarrior724@outlook.com>
2024-09-13 19:47:04 -0600

    fix:Ghosts advance 10 to be able to turn

--- Actividad3/pacman.py
+++ Actividad3/pacman.py
@@ -20,10 +20,10 @@ writer = Turtle(visible=False)
 aim = vector(5, 0)
 pacman = vector(-40, -80)
 ghosts = [
-    [vector(-180, 160), vector(5, 0)],
-    [vector(-180, -160), vector(0, 5)],
-    [vector(100, 160), vector(0, -5)],
-    [vector(100, -160), vector(-5, 0)],
+    [vector(-180, 160), vector(10, 0)],
+    [vector(-180, -160), vector(0, 10)],
+    [vector(100, 160), vector(0, -10)],
+    [vector(100, -160), vector(-10, 0)],
 ]
 # fmt: off
 tiles = [
@@ -135,14 +135,14 @@ def move():
             point.move(course)
         else:
             options = [
-                vector(5, 0),
-                vector(-5, 0),
-                vector(0, 5),
-                vector(0, -5),
+                vector(10, 0),
+                vector(-10, 0),
+                vector(0, 10),
+                vector(0, -10),
             ]
             plan = choice(options)
-            course.x = plan.x * 3
-            course.y = plan.y * 3
+            course.x = plan.x
+            course.y = plan.y
 
         up()
         goto(point.x + 10, point.y + 10)
