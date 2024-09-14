499285f7395c3a60d05809863fbe50a68964e6f6
BassedWarrior <bassedwarrior724@outlook.com>
2024-09-13 18:36:46 -0600

    feat:Faster projectile

--- Actividad4/cannon.py
+++ Actividad4/cannon.py
@@ -23,8 +23,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (x + 200) / 10
+        speed.y = (y + 200) / 10
 
 
 def inside(xy):
